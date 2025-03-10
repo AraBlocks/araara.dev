18a705d7c827a03b6e0be446475c3ba77cfcada8
Kevin Faaborg <zootella@zootella.com>
2025-03-09 23:09:13 -0400

    wording

--- app.vue
+++ app.vue
@@ -1,7 +1,7 @@
 <template>
 	<div>
 		<code>
-			<p>You're at araara.dev, an official domain of the Ara project and $ARA token.</p>
+			<p>This is araara.dev, an official domain of the Ara project and $ARA token.</p>
 			<p>Visit the official web page at <a href="https://ara.team">ara.team</a></p>
 			<p>The web2 foundation of this code and product brainstorm will be built on Nuxt and Vue:</p>
 		</code>
